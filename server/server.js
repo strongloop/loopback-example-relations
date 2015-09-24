f34010eef3f3e45274414e9e60407b27860eb1ec
crandmck <rand@strongloop.com>
2015-09-24 15:00:57 -0700

    Upgrade LB Explorer and related changes.

--- server/server.js
+++ server/server.js
@@ -4,33 +4,38 @@ var path = require('path');
 
 var app = module.exports = loopback();
 
-// Bootstrap the application, configure models, datasources and middleware.
-// Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname);
-
-app.set('view engine', 'ejs'); // LoopBack comes with EJS out-of-box
-app.set('json spaces', 2); // format json responses for easier viewing
-
-// must be set to serve views properly when starting the app via `slc run` from
-// the project root
-app.set('views', path.resolve(__dirname, 'views'));
-
-app.use('/', function(req, res, next) {
-  app.models.Customer.findOne(function(err, customer) {
-    if (err) return next(err);
-    res.render('index', {customer: customer});
-  });
-});
-
 app.start = function() {
   // start the web server
   return app.listen(function() {
     app.emit('started');
-    console.log('Web server listening at: %s', app.get('url'));
+    var baseUrl = app.get('url').replace(/\/$/, '');
+    console.log('Web server listening at: %s', baseUrl);
+    if (app.get('loopback-component-explorer')) {
+      var explorerPath = app.get('loopback-component-explorer').mountPath;
+      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+    }
   });
 };
 
-// start the server if `$ node server.js`
-if (require.main === module) {
-  app.start();
-}
+// Bootstrap the application, configure models, datasources and middleware.
+boot(app, __dirname, function(err) {
+  if (err) throw err;
+
+  app.set('view engine', 'ejs'); // LoopBack comes with EJS out-of-box
+  app.set('json spaces', 2); // format json responses for easier viewing
+
+  // must be set to serve views properly when starting from project root
+  app.set('views', path.resolve(__dirname, 'views'));
+
+  app.use('/', function(req, res, next) {
+    app.models.Customer.findOne(function(err, customer) {
+      if (err) return next(err);
+      res.render('index', {customer: customer});
+    });
+  });
+
+  // start the server if `$ node server.js`
+  if (require.main === module)
+    app.start();
+});
+
