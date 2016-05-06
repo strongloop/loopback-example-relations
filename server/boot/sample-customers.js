630aabde0ce37f18f69fc5b0953db60d3e165a83
Ryan Graham <r.m.graham@gmail.com>
2016-05-06 12:30:48 -0700

    update copyright notices and license

--- server/boot/sample-customers.js
+++ server/boot/sample-customers.js
@@ -1,3 +1,8 @@
+// Copyright IBM Corp. 2015,2016. All Rights Reserved.
+// Node module: loopback-example-relations
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
 module.exports = function(app) {
   var Customer = app.models.Customer;
   var Order = app.models.Order;
--- server/boot/z-customer-emails.js
+++ server/boot/z-customer-emails.js
@@ -1,3 +1,8 @@
+// Copyright IBM Corp. 2015. All Rights Reserved.
+// Node module: loopback-example-relations
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
 var async = require('async');
 
 module.exports = function(app, cb) {
